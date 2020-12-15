9db9681b532ee1a8be0ddc6327d8837087ab198b
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-12-15 12:24:33 -0600

    Hamburger App: Created layout component.

--- Hamburger/src/App.js
+++ Hamburger/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import Layout from './Components/Layout/Layout';
+
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hamburger</h1>
+        <Layout></Layout>
       </div>
     );
   }
