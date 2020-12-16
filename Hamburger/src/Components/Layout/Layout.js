4f20f8f47a8dd5fa0b2c43b866ee1f15beec215e
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-12-16 14:44:37 -0600

    Hamburger App: Added BurgerIngredient component.

--- Hamburger/src/Components/Layout/Layout.js
+++ Hamburger/src/Components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Aux from '../../Hoc/Aux';
+import classes from './Layout.css';
 
 const layout = (props) => (
   <Aux>
-    <div>Toolbar, SideDrawe, Backdrop</div>
-    <main>{props.children}</main>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
