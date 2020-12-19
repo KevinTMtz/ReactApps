8a1e4e7d5d82dce29b8b2f5bb6b96c18d14a1526
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-12-18 22:35:22 -0600

    Hamburger App: Created Burger component.

--- Hamburger/src/Containers/BurgerBuilder/BurgerBuilder.js
+++ Hamburger/src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
 import Aux from '../../Hoc/Aux';
+import Burger from '../../Components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  state = { ingredients: { bacon: 1, salad: 1, cheese: 1, meat: 2 } };
+
   render() {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Aux>
     );
