b4146fb94068cb38273d8de18a0961a43aa9c497
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-12-19 21:58:58 -0600

    Hamburger App: Created build controls & Improved styles.

--- Hamburger/src/Containers/BurgerBuilder/BurgerBuilder.js
+++ Hamburger/src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 
 import Aux from '../../Hoc/Aux';
 import Burger from '../../Components/Burger/Burger';
+import BuildControls from '../../Components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
-  state = { ingredients: { bacon: 1, salad: 1, cheese: 1, meat: 2 } };
+  state = { ingredients: { bacon: 0, salad: 0, cheese: 0, meat: 0 } };
 
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls />
       </Aux>
     );
   }
