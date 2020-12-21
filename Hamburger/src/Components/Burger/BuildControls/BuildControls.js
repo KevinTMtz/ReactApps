2ab9f7d87cfd7f13c2a3495bf484f2ec2404019e
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-12-21 16:28:59 -0600

    Hamburger App: Added order button & Order summary components.

--- Hamburger/src/Components/Burger/BuildControls/BuildControls.js
+++ Hamburger/src/Components/Burger/BuildControls/BuildControls.js
@@ -4,24 +4,32 @@ import classes from './BuildControls.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-  { label: 'Salad', type: 'salad' },
   { label: 'Bacon', type: 'bacon' },
+  { label: 'Salad', type: 'salad' },
   { label: 'Cheese', type: 'cheese' },
   { label: 'Meat', type: 'meat' },
 ];
 
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
-    <h3>Total price: ${props.price}</h3>
+    <h3 className={classes.TotalPrice}>
+      Total price: ${props.price}
+    </h3>
     {controls.map((control) => (
       <BuildControl
         key={control.label}
         label={control.label}
-        added={() => props.ingredientAdded(control.type)}
         removed={() => props.ingredientRemoved(control.type)}
+        added={() => props.ingredientAdded(control.type)}
         disabled={props.disabled[control.type]}
       />
     ))}
+    <button
+      className={classes.OrderButton}
+      disabled={props.unpurchaseable}
+    >
+      Order now
+    </button>
   </div>
 );
 
