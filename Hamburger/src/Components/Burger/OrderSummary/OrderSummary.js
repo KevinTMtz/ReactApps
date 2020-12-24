62e1f8f3f31c050ce5c11cb8d9242b96187064a9
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-12-24 16:05:38 -0600

    Hamburger App: Backdrop & Order summary buttons.

--- Hamburger/src/Components/Burger/OrderSummary/OrderSummary.js
+++ Hamburger/src/Components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Aux from '../../../Hoc/Aux';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map(
@@ -18,6 +19,12 @@ const orderSummary = (props) => {
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
+      <Button buttonType={'Danger'} clicked={props.purchaseCancel}>
+        Cancel
+      </Button>
+      <Button buttonType={'Success'} clicked={props.purchaseContinue}>
+        Continue
+      </Button>
     </Aux>
   );
 };
