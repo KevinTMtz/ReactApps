9816a36a07dbd6361350e5b1006fffb076d946b5
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-12-28 17:01:07 -0600

    Hamburger App: Changed folder structure.

--- Hamburger/src/Components/Burger/OrderSummary/OrderSummary.js
+++ Hamburger/src/Components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Aux from '../../../Hoc/Aux';
+import Aux from '../../../Hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
