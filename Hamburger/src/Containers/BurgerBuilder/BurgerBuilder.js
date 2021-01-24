e6398d629acac438f32a7125dd97cec3189cac45
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2021-01-24 17:16:51 -0600

    Hamburger App: Routing & Dynamic checkout.

--- Hamburger/src/Containers/BurgerBuilder/BurgerBuilder.js
+++ Hamburger/src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -86,29 +86,42 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true });
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   totalPrice: this.state.totalPrice.toFixed(2),
+    //   customer: {
+    //     name: 'Kevin',
+    //     address: { planet: 'Mandalore', zipCode: '12345' },
+    //     email: 'kevin@hamburger.app',
+    //   },
+    // };
+    // axios
+    //   .post('/orders.json', order)
+    //   .then((response) => {
+    //     this.setState({ loading: false, purchasing: false });
+    //     if (response.statusText === 'OK') {
+    //       alert('Your order has been taken!');
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   });
 
-    const order = {
-      ingredients: this.state.ingredients,
-      totalPrice: this.state.totalPrice.toFixed(2),
-      customer: {
-        name: 'Kevin',
-        address: { planet: 'Mandalore', zipCode: '12345' },
-        email: 'kevin@hamburger.app',
-      },
-    };
-    axios
-      .post('/orders.json', order)
-      .then((response) => {
-        this.setState({ loading: false, purchasing: false });
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(i) +
+          '=' +
+          encodeURIComponent(this.state.ingredients[i])
+      );
+    }
+    const queryString = queryParams.join('&');
 
-        if (response.statusText === 'OK') {
-          alert('Your order has been taken!');
-        }
-      })
-      .catch((error) => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString,
+    });
   };
 
   render() {
