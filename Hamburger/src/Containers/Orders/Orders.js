a6279a9eb137de4615ad98ec2188a089ffa6c559
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2021-01-25 20:49:44 -0600

    Hamburger App: Orders fetching & Some fixes.

--- Hamburger/src/Containers/Orders/Orders.js
+++ Hamburger/src/Containers/Orders/Orders.js
@@ -2,8 +2,30 @@ import React, { Component } from 'react';
 
 import Order from '../../Components/Order/Order';
 import classes from './Orders.css';
+import axios from '../../axios-orders';
+import WithErrorHandler from '../../Hoc/WithErrorHandler/WithErrorHandler';
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    axios
+      .get('/orders.json')
+      .then((res) => {
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({ ...res.data[key], id: key });
+        }
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch((error) => {
+        this.setState({ loading: false });
+      });
+  }
+
   render() {
     return (
       <div className={classes.Orders}>
@@ -15,4 +37,4 @@ class Orders extends Component {
   }
 }
 
-export default Orders;
+export default WithErrorHandler(Orders, axios);
