fddab5a9db610077e1f7b4cf9aed1d708077f2ef
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2021-01-30 23:25:16 -0600

    Frameworks App: Fetch friends & Display with Flatlist.

--- Frameworks App/App.tsx
+++ Frameworks App/App.tsx
@@ -1,21 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  FlatList,
+} from 'react-native';
 
 export default function App() {
+  const [myFriendsArray, setMyFriendsArray] = useState();
+
+  useEffect(() => {
+    fetch(
+      'https://raw.githubusercontent.com/KevinTMtz/AndroidApps/master/Friends.json'
+    )
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setMyFriendsArray(responseJson);
+      })
+      .catch((e) => alert(e));
+  }, []);
+
+  const renderItem = (itemData: any) => (
+    <Row friend={itemData.item} />
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>My Friends</Text>
+      <FlatList
+        style={styles.myFriendsContainer}
+        data={myFriendsArray}
+        renderItem={(item) => renderItem(item)}
+        keyExtractor={(friend) => friend.name}
+      />
+    </SafeAreaView>
   );
 }
 
+const Row = (props: any) => {
+  return (
+    <TouchableOpacity
+      style={styles.myFriendRow}
+      onPress={() =>
+        alert(
+          `Name: ${props.friend.name}\n` +
+            `Hobby: ${props.friend.hobby}\n` +
+            `Age: ${props.friend.age}\n` +
+            `Phone: ${props.friend.phone}\n` +
+            `Address: ${props.friend.address}\n`
+        )
+      }
+    >
+      <Text style={styles.rowTitle}>{props.friend.name}</Text>
+      <Text>{`Hobby: ${props.friend.hobby}`}</Text>
+    </TouchableOpacity>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
+    paddingBottom: 64,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  title: {
+    marginTop: 16,
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  myFriendsContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: 16,
+  },
+  myFriendRow: {
+    borderWidth: 1,
+    borderColor: '#b0b0b0',
+    marginBottom: 16,
+    padding: 16,
+    borderRadius: 10,
+  },
+  rowTitle: {
+    fontWeight: 'bold',
+    marginBottom: 4,
+    fontSize: 18,
   },
 });
