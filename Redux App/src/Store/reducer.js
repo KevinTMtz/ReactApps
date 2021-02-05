81485e6bc4b27ae37f7f84611bb7568a5445bac7
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2021-02-05 16:38:56 -0600

    Redux App: Store & remove counter values.

--- Redux App/src/Store/reducer.js
+++ Redux App/src/Store/reducer.js
@@ -1,16 +1,31 @@
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
+const STORE_RESULT = 'STORE_RESULT';
+const DELETE_RESULT = 'DELETE_RESULT';
 
 const initialState = {
   counter: 0,
+  results: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-      return { counter: state.counter + action.value };
+      return { ...state, counter: state.counter + action.value };
     case DECREMENT:
-      return { counter: state.counter - action.value };
+      return { ...state, counter: state.counter - action.value };
+    case STORE_RESULT:
+      return {
+        ...state,
+        results: state.results.concat(state.counter),
+      };
+    case DELETE_RESULT:
+      return {
+        ...state,
+        results: state.results.filter(
+          (result, index) => index !== action.index
+        ),
+      };
     default:
       return state;
   }
