c0827e2141ecd631355b063612fe9c7d16a555ca
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2021-02-11 14:45:10 -0600

    Hamburger App: Added Redux thunk.

--- Hamburger/src/index.js
+++ Hamburger/src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import reducer from './Store/Reducers/burgerBuilder';
+import burgerBuilderReducer from './Store/Reducers/burgerBuilder';
 
-const store = createStore(reducer);
+const store = createStore(
+  burgerBuilderReducer,
+  compose(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
