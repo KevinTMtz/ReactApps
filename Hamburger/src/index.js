491e68481bdd195cfb642280ba9ae92db21acd15
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2021-02-12 14:30:45 -0600

    Hamburger App: Combined reducers.

--- Hamburger/src/index.js
+++ Hamburger/src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import {
+  createStore,
+  applyMiddleware,
+  compose,
+  combineReducers,
+} from 'redux';
 import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import burgerBuilderReducer from './Store/Reducers/burgerBuilder';
+import orderReducer from './Store/Reducers/order';
+
+const rootReducer = combineReducers({
+  burgerBuilder: burgerBuilderReducer,
+  order: orderReducer,
+});
 
 const store = createStore(
-  burgerBuilderReducer,
+  rootReducer,
   compose(applyMiddleware(thunk))
 );
 
