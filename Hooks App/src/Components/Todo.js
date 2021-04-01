e86389456ac46fa5069ba9acbcdcb6311b81109c
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2021-04-01 14:54:52 -0600

    Hooks App: Save todo list with Firebase realtime database.

--- Hooks App/src/Components/Todo.js
+++ Hooks App/src/Components/Todo.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
-const Todo = (props) => {
+const Todo = () => {
   const [inputState, setInputState] = useState('');
   const [todoList, setTodoList] = useState(['Code all day!']);
 
@@ -12,17 +13,28 @@ const Todo = (props) => {
         onChange={(event) => setInputState(event.target.value)}
         value={inputState}
       />
+
       <br />
+
       <button
         type='text'
         onClick={() => {
-          setTodoList(todoList.concat(inputState));
-          setInputState('');
+          axios
+            .post(
+              'https://hooks-app-131b4-default-rtdb.firebaseio.com/todoListItems.json',
+              { name: inputState }
+            )
+            .then(() => {
+              setTodoList(todoList.concat(inputState));
+              setInputState('');
+            })
+            .catch((error) => alert(error));
         }}
         disabled={inputState === ''}
       >
         Add
       </button>
+
       <ul>
         {todoList.map((todo, index) => (
           <li key={index}>{todo}</li>
