3d5bb654fa9de7339452ba19d94a9fb8430d1234
KevinTMtz <torresmtzkevin@gmail.com>
2021-06-17 22:20:08 -0500

    Hamburger App: BurgerIngredient, OrderSummary & Modal classes to functional components.

--- Hamburger/src/Components/Burger/OrderSummary/OrderSummary.js
+++ Hamburger/src/Components/Burger/OrderSummary/OrderSummary.js
@@ -1,43 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Aux from '../../../Hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-class OrderSummary extends Component {
-  render() {
-    const ingredientSummary = Object.keys(this.props.ingredients).map(
-      (igKey, index) => (
-        <li key={igKey}>
-          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>
-          : {this.props.ingredients[igKey]}
-        </li>
-      )
-    );
+const OrderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => (
+    <li key={igKey}>
+      <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
+      {props.ingredients[igKey]}
+    </li>
+  ));
 
-    return (
-      <Aux>
-        <h3>Your order</h3>
-        <p>A delicious burger with the following ingredients:</p>
-        <ul>{ingredientSummary}</ul>
-        <p>
-          <strong>Total price: ${this.props.totalPrice}</strong>
-        </p>
-        <p>Continue to Checkout?</p>
-        <Button
-          buttonType={'Danger'}
-          clicked={this.props.purchaseCancel}
-        >
-          Cancel
-        </Button>
-        <Button
-          buttonType={'Success'}
-          clicked={this.props.purchaseContinue}
-        >
-          Continue
-        </Button>
-      </Aux>
-    );
-  }
-}
+  return (
+    <Aux>
+      <h3>Your order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total price: ${props.totalPrice}</strong>
+      </p>
+      <p>Continue to Checkout?</p>
+      <Button buttonType={'Danger'} clicked={props.purchaseCancel}>
+        Cancel
+      </Button>
+      <Button buttonType={'Success'} clicked={props.purchaseContinue}>
+        Continue
+      </Button>
+    </Aux>
+  );
+};
 
 export default OrderSummary;
--- Hamburger/src/Components/UI/Modal/Modal.js
+++ Hamburger/src/Components/UI/Modal/Modal.js
@@ -1,39 +1,30 @@
-import React, { Component } from 'react';
+import React, { memo } from 'react';
 
 import classes from './Modal.css';
 import Aux from '../../../Hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-class Modal extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return (
-      nextProps.show !== this.props.show ||
-      nextProps.children !== this.props.children
-    );
-  }
+const Modal = (props) => {
+  return (
+    <Aux>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div
+        className={classes.Modal}
+        style={{
+          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: props.show ? '1' : '0',
+          height: props.show ? 'auto' : '0px',
+        }}
+      >
+        {props.children}
+      </div>
+    </Aux>
+  );
+};
 
-  render() {
-    return (
-      <Aux>
-        <Backdrop
-          show={this.props.show}
-          clicked={this.props.modalClosed}
-        />
-        <div
-          className={classes.Modal}
-          style={{
-            transform: this.props.show
-              ? 'translateY(0)'
-              : 'translateY(-100vh)',
-            opacity: this.props.show ? '1' : '0',
-            height: this.props.show ? 'auto' : '0px',
-          }}
-        >
-          {this.props.children}
-        </div>
-      </Aux>
-    );
-  }
-}
-
-export default Modal;
+export default memo(
+  Modal,
+  (prevProps, nextProps) =>
+    nextProps.show === prevProps.show &&
+    nextProps.children === prevProps.children,
+);
