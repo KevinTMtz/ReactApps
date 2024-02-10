b21df1768eefa313f10aaaec59295519d59cf40a
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2024-02-09 21:02:37 -0600

    Created React Essentials App

---
+++ React Essentials/src/App.tsx
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+
+const App = () => (
+  <div>
+    <Header />
+    <main>
+      <h2>Time to get started!</h2>
+    </main>
+  </div>
+);
+
+export default App;
